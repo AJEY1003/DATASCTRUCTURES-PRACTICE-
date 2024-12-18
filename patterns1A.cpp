c3b07ca32f230c86963d476d3a40189a982b1b00
AJEY K <145177735+AJEY1003@users.noreply.github.com>
2024-12-18 22:04:59 +0530

    Create patterns1A.cpp

---
+++ patterns1A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int len;
+    int breadth;
+    cin>>len;
+    cin>>breadth;
+    for(int i=1;i<=len;i++){
+        for(int j=1;j<=breadth;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    
+}
